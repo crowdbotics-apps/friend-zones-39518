f8d3bf026556a54c691afd74a97670714db1b3ab
Crowdbotics <team@crowdbotics.com>
2023-03-23 05:17:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "friend-zones-39518",
+  url: "https://friend-zones-39518.botics.co",
+  api: "https://friend-zones-39518.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
